605f523e8706432dc2d3c62f666f522089675625
Andrew Hooper <ahooper@kos.net>
2023-04-29 11:02:33 -0400

    tidy files again

--- try_mach_time.swift
+++
@@ -1,7 +0,0 @@
-import Darwin
-var timebaseInfo = mach_timebase_info_data_t()
-mach_timebase_info(&timebaseInfo)
-print(timebaseInfo)
-let start=mach_absolute_time();sleep(1);let stop=mach_absolute_time()
-print(stop-start)
-print((stop-start) * UInt64(timebaseInfo.numer) / UInt64(timebaseInfo.denom) / 1000000, "μSec")
